46add0f1b67707ea634756682b02ab10677eae72
drb27 <david.bradshaw.usa@gmail.com>
2015-02-25 06:43:53 -0500

    Added i_worker

--- src/stock/worker_base.h
+++ src/stock/worker_base.h
@@ -10,6 +10,24 @@ using std::ostream;
 using std::function;
 
 /**
+ * Interface definition for a class which can perform an action 
+ * either synchronously, or asynchronously.
+ */
+template<class T>
+class i_worker
+{
+
+public:
+
+virtual T perform_sync() =0;
+virtual void perform_async() =0;
+virtual bool has_performed() const =0;
+virtual T retrieve_async() const =0;
+virtual T wait() const =0;
+
+};
+
+/**
  * Simple implemetation of a worker thread.
  *
  * Designed to be used with C++11 threads. This is an abstract class,
