9ab480c449462a60c389a13297749b1e8c6ad06b
drb27 <david.bradshaw.usa@gmail.com>
2015-02-25 08:56:24 -0500

    WIP

--- src/stock/worker_base.h
+++ src/stock/worker_base.h
@@ -4,27 +4,67 @@
 #include <string>
 #include <ostream>
 #include <functional>
+#include <thread>
+#include <type_traits>
 
 using std::string;
 using std::ostream;
 using std::function;
+
+enum class WorkResult
+{
+    NotPerformed=0,
+	Success,
+	Failure
+	};
+
+/**
+ * Interface definition for a class which can return a result.
+ * The result type is parameterized.
+ * 
+ * @param R The type of the result to be returned.
+ *
+ * @note The default value of the result immediately after construction is zero,
+ *       regardless of whether or not this is a valid  
+ */
+template<class R>
+class i_resultor
+{
+    static_assert(std::is_enum<R>::value,"i_resultor can only be used with enumerations");
+
+public:
+    /**
+     * Returns a reference to the current result.
+     */
+    const R& result() const { return _result; }
+    
+    /**
+     * Returns a reference to the current exception.
+     * Typically only useful if the result code indicates a failure
+     */
+    const std::exception& error() const { return _exception; }
+
+protected:
+    R _result = R();
+    std::exception _exception = std::exception();
+};
 
 /**
  * Interface definition for a class which can perform an action 
  * either synchronously, or asynchronously.
  */
 template<class T>
-class i_worker
+class i_worker : public i_resultor<WorkResult>
 {
-
+    
 public:
-
-virtual T perform_sync() =0;
-virtual void perform_async() =0;
-virtual bool has_performed() const =0;
-virtual T retrieve_async() const =0;
-virtual T wait() const =0;
-
+    
+    virtual T perform_sync() =0;
+    virtual void perform_async() =0;
+    virtual bool has_performed() const =0;
+    virtual T retrieve_async() const =0;
+    virtual T wait() const =0;
+    
 };
 
 /**
@@ -47,8 +87,8 @@ class worker_base
 {
 public:
 
-    worker_base();
-    virtual void operator()(int) final;
+worker_base();
+virtual void operator()(int) final;
     virtual void do_work(int)=0;
     
     virtual void set_completion_action( function<void()> f ) final;
