7010ca2179318598b4d2a30565cb79d217a896fe
drb27 <david.bradshaw.usa@gmail.com>
2015-03-17 07:28:40 -0400

    Added stockchart background

--- src/stockgui/stockchart.cpp
+++ src/stockgui/stockchart.cpp
@@ -33,6 +33,20 @@ SOFTWARE.
 
 G_DEFINE_TYPE (GtkStockChart, stock_chart, GTK_TYPE_DRAWING_AREA);
 
+static void draw_background(cairo_t* cr,guint width, guint height)
+{
+    cairo_pattern_t* pat;
+    cairo_save(cr);
+    pat=cairo_pattern_create_linear(width/2-5,5, width/2-5, height-5);
+    cairo_pattern_add_color_stop_rgba(pat, 0.0, 0.0, 0.07, 0.09, 0.5);
+    cairo_pattern_add_color_stop_rgba(pat, 1.0, 0.0, 0.0, 0.0, 0.9);
+    cairo_set_source(cr, pat);
+    cairo_rectangle(cr, 0,0, width, height);
+    cairo_fill(cr);
+    cairo_restore(cr);
+}
+
+
 static gboolean stock_chart_draw(GtkWidget* w, cairo_t* cr)
 {
     guint width = gtk_widget_get_allocated_width(w);
@@ -42,9 +56,13 @@ static gboolean stock_chart_draw(GtkWidget* w, cairo_t* cr)
     x = width / 2;
     y = height / 2;
 
+    draw_background(cr,width,height);
+
     double radius;
     radius = MIN (width / 2,
                   height / 2) - 5;
+
+    cairo_set_line_width(cr,1.0);
 
     /* M_PI - a macro to express the value of pi (precision?) - provided by math.h */
     cairo_arc (cr, x, y, radius, 0, 2 * M_PI);
