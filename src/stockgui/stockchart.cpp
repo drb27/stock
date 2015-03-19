484e04fff18f89c83d705aa30077a24f79cef0be
drb27 <david.bradshaw.usa@gmail.com>
2015-03-19 08:06:56 -0400

    Added a grid

--- src/stockgui/stockchart.cpp
+++ src/stockgui/stockchart.cpp
@@ -33,17 +33,71 @@ SOFTWARE.
 
 G_DEFINE_TYPE (GtkStockChart, stock_chart, GTK_TYPE_DRAWING_AREA);
 
-static void draw_background(cairo_t* cr,guint width, guint height)
+class point
+{
+public:
+    point(double xx,double yy) : x(xx), y(yy) {}
+    const double x{0},y{0};
+};
+
+class rect
+{
+public:
+    rect(double xx, double yy, double ww, double hh) : origin{xx,yy},width(ww),height(hh) {}
+    const point origin{0.0,0.0};
+    const double width{0},height{0};
+
+    double top() const { return origin.y+height; }
+    double bottom() const { return origin.y; }
+    double left() const { return origin.x; }
+    double right() const { return origin.x+width; }
+    
+    double area() const { return width*height; }
+    
+    point center() const { return point(origin.x + width/2.0, origin.y + height/2); }
+    point bottomleft() const { return origin; }
+    point bottomright() const { return point(origin.x + width, origin.y); }
+    point topleft() const { return point(origin.x, origin.y + height); }
+    point topright() const { return point(origin.x + width, origin.y + height); }
+
+    rect inset(double i) const { return rect(origin.x+i,origin.y+i,width-2*i,height-2*i); }
+    rect offset( const point& p ) const { return rect(origin.x+p.x,origin.y+p.y,width,height); }
+};
+
+static void draw_background(cairo_t* cr, rect r)
 {
     cairo_pattern_t* pat;
     cairo_save(cr);
-    pat=cairo_pattern_create_linear(width/2-5,5, width/2-5, height-5);
-    cairo_pattern_add_color_stop_rgba(pat, 0.0, 0.0, 0.07, 0.09, 0.5);
-    cairo_pattern_add_color_stop_rgba(pat, 1.0, 0.0, 0.0, 0.0, 0.9);
+    pat=cairo_pattern_create_linear(r.width/2-5,5, r.width/2-5, r.height-5);
+    cairo_pattern_add_color_stop_rgba(pat, 0.0, 0.4, 0.4, 0.4, 1.0);
+    cairo_pattern_add_color_stop_rgba(pat, 1.0, 0.0, 0.0, 0.0, 1.0);
     cairo_set_source(cr, pat);
-    cairo_rectangle(cr, 0,0, width, height);
+    cairo_rectangle(cr, r.left(),r.bottom(), r.width, r.height);
     cairo_fill(cr);
     cairo_restore(cr);
+}
+
+static void draw_grid(cairo_t* cr,const rect& area, guint divs_x, guint divs_y)
+{
+    cairo_set_line_width(cr,1.0);
+    cairo_set_source_rgb(cr, 1, 1, 1);
+
+    double x_size = area.width / ((double)divs_x);
+    double y_size = area.height / ((double)divs_y);
+
+    for ( double x = area.left(); x < (area.right() + 0.5*x_size ); x+= x_size )
+    {
+	cairo_move_to(cr,x,area.bottom());
+	cairo_line_to(cr,x,area.top());
+    }
+
+    for ( double y = area.bottom(); y < ( area.top() + 0.5*y_size ); y+= y_size )
+    {
+	cairo_move_to(cr,area.left(),y);
+	cairo_line_to(cr,area.right(),y);
+    }
+
+    cairo_stroke(cr);
 }
 
 
@@ -52,39 +106,14 @@ static gboolean stock_chart_draw(GtkWidget* w, cairo_t* cr)
     guint width = gtk_widget_get_allocated_width(w);
     guint height = gtk_widget_get_allocated_height(w);
 
+    rect area(0.0,0.0,width,height);
+
     double x, y;
     x = width / 2;
     y = height / 2;
 
-    draw_background(cr,width,height);
-
-    double radius;
-    radius = MIN (width / 2,
-                  height / 2) - 5;
-
-    cairo_set_line_width(cr,1.0);
-
-    /* M_PI - a macro to express the value of pi (precision?) - provided by math.h */
-    cairo_arc (cr, x, y, radius, 0, 2 * M_PI);
-
-    cairo_set_source_rgb (cr, 1, 1, 1);
-    cairo_fill_preserve (cr);
-    cairo_set_source_rgb (cr, 0, 0, 0);
-    cairo_stroke (cr);
-
-    int i;
-    for (i = 0; i < 12; i++)
-    {
-        int inset;
-        inset = 0.1 * radius;
-        cairo_move_to (cr,
-                       x + (radius - inset) * cos (i * M_PI / 6),
-                       y + (radius - inset) * sin (i * M_PI / 6));
-        cairo_line_to (cr,
-                       x + radius * cos (i * M_PI / 6),
-                       y + radius * sin (i * M_PI / 6));
-        cairo_stroke (cr);
-    } 
+    draw_background(cr,area);
+    draw_grid(cr,area.inset(10),10,10);
 
     return FALSE;
 }
